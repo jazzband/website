f310d5882bab44e965b049efa3e9a45b5e60d30f
Jannis Leidel <jannis@leidel.info>
2016-04-21 13:08:49 +0200

    Proxy fix for GitHub webhooks.

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import os
 from flask.ext.assets import ManageAssets
 from flask.ext.script import Manager
 from flask_migrate import MigrateCommand
+from werkzeug.contrib.fixers import ProxyFix
 
 from jazzband.app import create_app
 from jazzband.assets import assets
@@ -9,6 +11,8 @@ from jazzband.commands import SyncMembers, SyncProjects
 
 
 app = create_app('jazzband.config')
+if 'HEROKU_APP_NAME' in os.environ:
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
 manager = Manager(app)
 manager.add_command('assets', ManageAssets(assets))
