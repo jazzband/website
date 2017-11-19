3f658e1441b8bfa3c31bd5f97a644dbdd9d537a1
Jannis Leidel <jannis@leidel.info>
2017-11-20 00:00:28 +0100

    Fix culprit.

--- jazzband/errors.py
+++ jazzband/errors.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 if 'SENTRY_DSN' in os.environ:
     from raven.contrib.flask import Sentry
-    sentry = Sentry(logging=True, logging_level=logging.INFO)
+    sentry = Sentry(logging=True, level=logging.INFO)
 else:
     class FakeSentry:
         def init_app(self, *args, **kwargs):
