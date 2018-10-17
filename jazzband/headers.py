19bc03ab79f368e5909aff8559e380d3246af78e
Jannis Leidel <jannis@leidel.info>
2018-10-17 13:23:41 +0200

    Disable CSP in flask-admin.

--- jazzband/headers.py
+++ jazzband/headers.py
@@ -1,4 +1,12 @@
+from flask import request
 from flask_talisman import Talisman
 
 
-talisman = Talisman()
+class JazzbandTalisman(Talisman):
+    def _set_content_security_policy_headers(self, headers, options):
+        if request.path.startswith('/admin'):
+            options['content_security_policy'] = None
+        return super()._set_content_security_policy_headers(headers, options)
+
+
+talisman = JazzbandTalisman()
