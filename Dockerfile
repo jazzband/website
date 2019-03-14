1c5334c657bcf3dec7903399a69f7fc7ac583d9b
Jannis Leidel <jannis@leidel.info>
2019-03-14 10:38:50 +0100

    Use wait-for-it Debian package.

--- Dockerfile
+++ Dockerfile
@@ -30,7 +30,7 @@ RUN apt-get update && \
     apt-get install -y --no-install-recommends \
         build-essential curl git libpq-dev \
         postgresql-client gettext sqlite3 libffi-dev \
-        inotify-tools wget bzip2 && \
+        inotify-tools wget bzip2 wait-for-it && \
     apt-get autoremove -y && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,23 +3,11 @@ set -eo pipefail
 
 # default variables
 : "${PORT:=5000}"
-: "${SLEEP:=1}"
-: "${TRIES:=60}"
-
-function wait_for_check {(
-  tries=0
-  echo "Waiting for $1 to respond..."
-  while true; do
-    [[ $tries -lt $TRIES ]] || return
-    flask check $1 >/dev/null 2>&1
-    [[ $? -eq 0 ]] && return
-    sleep $SLEEP
-    tries=$((tries + 1))
-  done
-)}
+: "${TIMEOUT:=60}"
 
 # first wait for the database
-wait_for_check redis & wait_for_check db
+wait-for-it redis:6379 -t ${TIMEOUT}
+wait-for-it db:5432 -t ${TIMEOUT}
 
 # then run the database migrations
 flask db upgrade
