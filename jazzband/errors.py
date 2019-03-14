2bad769e4fd47169e72792574c1c367e9fdfb4a4
Jannis Leidel <jannis@leidel.info>
2019-03-14 13:33:43 +0100

    Factory pattern.

--- jazzband/errors.py
+++ jazzband/errors.py
@@ -1,4 +1,5 @@
 import logging
+from flask import render_template
 
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
@@ -6,6 +7,19 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 
 
 def init_app(app):
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('error.html'), 404
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('forbidden.html'), 403
+
+    @app.errorhandler(500)
+    def error(error):
+        return render_template('error.html'), 500
+
     sentry_sdk.init(
         integrations=[
             LoggingIntegration(
