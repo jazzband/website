54995f0180177c38ff483d2d5c66925a1c274fb1
Jake Stockwin <jstockwin@gmail.com>
2020-10-02 12:17:58 +0100

    Update create issue links
    Closes https://github.com/jazzband-roadies/website/issues/730
    
    - The old logic would open an issue with a label get parameter, however
    this didn't work (see
    https://github.com/jazzband-roadies/help/issues/188)
    - Instead, we have added some issue templates
    (https://github.com/jazzband-roadies/help/pull/197)
    - The URL to view the template selection is /issues/new/choose
    - This PR removed the logic for handling, and instead takes users
    directly to the template selection page.
    - Strangely, choosing one of these templates seems to add most options
    into the get params (including labels), but somehow it gets around the
    permissions issue.
    - I have also changed the link for if the website is erroring to create
    an issue on the website repo, rather than the help repo.
    
    Note: I could have linked to the resulting issues page once you'd chosen
    your selection (e.g. "Project lead"), but there are multiple get
    parameters, and some would need upating if we change the template. I
    think it's better like this, so we can update the templates without
    making any other changes. Let me know if you disagree.

--- jazzband/members/views.py
+++ jazzband/members/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, request
+from flask import Blueprint, redirect
 from sqlalchemy.sql.expression import func
 
 from ..decorators import templated
@@ -21,8 +21,11 @@ def roadies():
 
 @members.route("/roadies/issue")
 def roadies_issue():
-    labels = request.args.get("labels", None)
-    url = "https://github.com/jazzband-roadies/help/issues/new"
-    if labels:
-        url += f"?labels={labels}"
+    url = "https://github.com/jazzband-roadies/help/issues/new/choose"
+    return redirect(url)
+
+
+@members.route("/website/issue")
+def website_issue():
+    url = "https://github.com/jazzband-roadies/website/issues/new"
     return redirect(url)
